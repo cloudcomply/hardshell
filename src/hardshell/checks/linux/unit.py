63864380d478e4e12579d67651d0e1d5921949ca
Tom Burge <tom@tomburge.org>
2024-06-28 10:33:12 -0500

    commenting out debug statements

--- src/hardshell/checks/linux/unit.py
+++ src/hardshell/checks/linux/unit.py
@@ -22,53 +22,43 @@ class UnitCheck(BaseCheck):
         if actual_state == expected_state:
             log_and_print(
                 f"Unit {unit_name} is {actual_state} and supposed to be {expected_state}.",
-                # log_only=True,
+                log_only=True,
             )
             return True
         else:
             log_and_print(
                 f"Unit {unit_name} is {actual_state} and supposed to be {expected_state}.",
                 level="error",
-                # log_only=True,
+                log_only=True,
             )
             return False
 
     def run_check(self, current_os, global_config):
-        log_and_print(
-            "running unit check",
-            #   log_only=True
-        )
+        log_and_print("running unit check", log_only=True)
         self.run_check_systemd()
 
     def run_check_systemd(self):
         log_and_print(
-            f"running unit check for systemd for {self.unit_name}",
-            #   log_only=True
+            f"running unit check for systemd for {self.unit_name}", log_only=True
         )
         try:
             manager = Manager()
             manager.load()
 
-            print(manager)
+            # print(manager)
 
             unit_exists = manager.GetUnit(self.unit_name.encode("utf-8"))
             unit_exists_decoded = unit_exists.decode("utf-8")
 
-            print(f"unit exists: {unit_exists}")
-            print(f"unit exists decoded: {unit_exists_decoded}")
+            # print(f"unit exists: {unit_exists}")
+            # print(f"unit exists decoded: {unit_exists_decoded}")
 
-            log_and_print(
-                f"unit exists: {unit_exists_decoded}",
-                #   log_only=True
-            )
+            log_and_print(f"unit exists: {unit_exists_decoded}", log_only=True)
 
             if unit_exists and os.path.exists(
                 f"/usr/lib/systemd/system/{self.unit_name}"
             ):
-                log_and_print(
-                    f"unit {self.unit_name} exists",
-                    # log_only=True
-                )
+                log_and_print(f"unit {self.unit_name} exists", log_only=True)
 
                 unit = Unit(self.unit_name.encode("utf-8"))
                 unit.load()
@@ -77,15 +67,15 @@ class UnitCheck(BaseCheck):
                 load_state = unit.LoadState.decode("utf-8")
                 unit_file_state = unit.UnitFileState.decode("utf-8")
 
-                print(f"active state: {active_state}")
-                print(f"load state: {load_state}")
-                print(f"unit file state: {unit_file_state}")
+                # print(f"active state: {active_state}")
+                # print(f"load state: {load_state}")
+                # print(f"unit file state: {unit_file_state}")
 
                 if load_state == "not-found":
                     log_and_print(
                         f"unit {self.unit_name} does not exist.",
                         level="error",
-                        # log_only=True
+                        log_only=True,
                     )
                     return
 
@@ -105,9 +95,9 @@ class UnitCheck(BaseCheck):
                     self.unit_name, self.unit_state, unit_file_state, "UnitFileState"
                 )
 
-                print(f"active status: {active_status}")
-                print(f"loaded status: {loaded_status}")
-                print(f"file status: {file_status}")
+                # print(f"active status: {active_status}")
+                # print(f"loaded status: {loaded_status}")
+                # print(f"file status: {file_status}")
 
                 self.set_result_and_log_status(
                     self.check_id,
@@ -119,26 +109,23 @@ class UnitCheck(BaseCheck):
                     self.check_type,
                 )
             else:
-                log_and_print(
-                    f"unit {self.unit_name} does not exist",
-                    # log_only=True
-                )
+                log_and_print(f"unit {self.unit_name} does not exist", log_only=True)
 
         except AttributeError as e:
             log_and_print(
                 f"unit {self.unit_name} does not exist: {e}",
                 level="error",
-                # log_only=True,
+                log_only=True,
             )
         except pystemd.dbusexc.DBusFileNotFoundError as e:
             log_and_print(
                 f"unit {self.unit_name} does not exist: {e}",
                 level="error",
-                # log_only=True,
+                log_only=True,
             )
         except pystemd.dbusexc.DBusNoSuchUnitError as e:
             log_and_print(
                 f"unit {self.unit_name} does not exist: {e}",
                 level="error",
-                # log_only=True,
+                log_only=True,
             )
